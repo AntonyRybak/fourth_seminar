a42dfcf9d23dc020ba7640cea3d0a62a8c35e210
Anton Rybak <toxa-wow@mail.ru>
2022-08-31 02:42:52 +0300

    task29 done

---
+++ task29/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите количество элементов массива ");
+int n = int.Parse(Console.ReadLine());
+int [] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите элемент массива под индексом {i} ");
+    array[i] = int.Parse(Console.ReadLine());
+}
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
