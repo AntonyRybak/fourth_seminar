2175bd81b52ba46bff77afbe01fba2e6c9d30b0a
Anton Rybak <toxa-wow@mail.ru>
2022-08-31 01:46:14 +0300

    task 25 done

---
+++ task25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Напишите число A ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Напишите число B ");
+int b = int.Parse(Console.ReadLine());
+int result = a;
+for (int i = 0; i < b - 1; i++)
+{
+    result = result * a;
+}
+ Console.WriteLine(result);
