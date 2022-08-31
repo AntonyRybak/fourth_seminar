c73908b249ed5649be3b3dcc5abcadc506b913d6
Anton Rybak <toxa-wow@mail.ru>
2022-08-31 03:11:34 +0300

    startask done

---
+++ startask/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int[] array = new int[10];
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    array[i] = new Random().Next(-10, 10);
+    Console.Write(array[i] + " ");
+}
+int max = array.Max();
+Console.WriteLine("Максимальное значение " + max);
+
